c04768871dfe87de79134b56e7a1fbacbb09b317
Henoc Musau <henocmusau@gmail.com>
2023-12-15 12:54:08 +0100

    Ready for first Stage to Vercel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ import LogoutButton from '../components/LogoutButton'
 export default function AdminPage() {
     return (
         <section className='flex flex-col items-center justify-center h-full text-left'>
-            <h1 className='text-3xl'>Very Secret Page with Nuclear codes</h1>
+            <h1 className='text-3xl'>Very Secret Page with Nuclear codes - HM</h1>
             <p className='my-6 text-red-400'>Code : CFF8563G7Bcs6sdZ8S455xd</p>
 
             <Link
